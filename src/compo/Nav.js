a6c08928419eec1ebf6f1645c167ce976d4e46ec
Om Prakash Mallik <omprakashmallik91@gmail.com>
2023-09-29 18:46:30 +0530

    added active affect on nav

--- src/compo/Nav.js
+++ src/compo/Nav.js
@@ -1,14 +1,15 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 
 function Nav() {
+  const location = useLocation();
   return (
     <div className="bg-green-500">
       <ul className="p-6 flex item-center gap-10 justify-center">
-        <Link to="/" className="font-bold text-lg ">Clock</Link>
-        <Link to="alarm" className="font-bold text-lg ">Alarm</Link>
-        <Link to="timer" className="font-bold text-lg ">Timer</Link>
-        <Link to="stopwatch" className="font-bold text-lg ">Stopwatch</Link>
+        <Link to="/" className={`hover:text-white active:text-white font-bold text-lg ${location.pathname === '/' ? 'text-white' : ''} `}>Clock</Link>
+        <Link to="alarm" className={`hover:text-white active:text-white font-bold text-lg ${location.pathname === '/alarm' ? 'text-white' : ''} `}>Alarm</Link>
+        <Link to="timer" className={`hover:text-white active:text-white font-bold text-lg ${location.pathname === '/timer' ? 'text-white' : ''} `}>Timer</Link>
+        <Link to="stopwatch" className={`hover:text-white active:text-white font-bold text-lg ${location.pathname === '/stopwatch' ? 'text-white' : ''} `}>Stopwatch</Link>
       </ul>
     </div>
   )
