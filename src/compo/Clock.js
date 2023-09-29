6a60bf9973bd2e4256ad681f448e4237a31020f3
Om Prakash Mallik <omprakashmallik91@gmail.com>
2023-09-30 04:43:04 +0530

    added clock

--- src/compo/Clock.js
+++ src/compo/Clock.js
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import './style.css'
 
-function Clock() {
+function DigitalClock() {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  const hours = time.getHours();
+  const minutes = time.getMinutes();
+  const seconds = time.getSeconds();
+
   return (
     <div className='p-5'>
-      <h1 className='text-green-700 font-bold text-center text-5xl'>Clock</h1>
+      <h1 className='text-green-700 font-bold text-center text-5xl'>Digital Clock</h1>
+      <div className="digital-clock text-white text-center">
+        <div className="time">
+          {hours < 10 ? `0${hours}` : hours}:
+          {minutes < 10 ? `0${minutes}` : minutes}:
+          {seconds < 10 ? `0${seconds}` : seconds}
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Clock
+export default DigitalClock;
--- src/compo/Footer.js
+++ src/compo/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div className="bg-green-500 p-5 align">
+    <div className="bg-green-500 p-4 fixed bottom-0 w-full">
       <p className="text-center font-bold">By: Om Prakash Mallik</p>
     </div>
   )
