d77927a666a9a23089f2c5e11938c1cf41e08e48
Om Prakash Mallik <opm202000@gmail.com>
2023-12-08 15:29:18 +0530

    testing

--- src/compo/Stopwatch.js
+++ src/compo/Stopwatch.js
@@ -5,6 +5,7 @@ function Stopwatch() {
   return (
     <div className='p-5'>
       <h1 className='text-white font-bold text-center text-5xl'>STOPWATCH</h1>
+      <p className='text-yellow-500 font-bold text-center text-5xl'>Under Construction</p>
     </div>
   )
 }
